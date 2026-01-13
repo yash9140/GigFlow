85aab95175919b610777d3021cabaedadc86ed20
YashDhanraj <yashdhanraj9140@gmail.com>
2026-01-14 00:07:52 +0530

    Add password hashing using bcrypt

--- backend/models/User.js
+++ backend/models/User.js
@@ -30,4 +30,21 @@ const userSchema = new mongoose.Schema(
     }
 );
 
+// Hash password before saving
+const bcrypt = require('bcryptjs');
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+// Compare password method
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
 module.exports = mongoose.model('User', userSchema);
